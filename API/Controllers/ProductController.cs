74c756d7df38f131ae2fec5dbc5503f1309445e9
Ahmed Sherif <ahmedsherif91101@gmail.com>
2024-09-10 14:55:07 +0300

    Data Seed , Eager and Lazy Loading

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts() 
+        {
+            IReadOnlyList<Product> products = await _productRepository.GetProductsAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            Product? product = await _productRepository.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+        [HttpGet("Types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+        {
+            IReadOnlyList<ProductType> productTypes = await _productRepository.GetProductTypesAsync();
+            return Ok(productTypes);
+        }
+        [HttpGet("Brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            IReadOnlyList<ProductBrand> productBrands = await _productRepository.GetProductBrandsAsync();
+            return Ok(productBrands);
+        }
+    }
+}
---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class BaseEntity
+    {  
+        // To make Entity Framework Understand it as Primary Key must Name Id 
+        public int Id { get; set; }
+    }
+}
